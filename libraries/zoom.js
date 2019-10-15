50450c4f5821315602be715a316254b6dda449e8
nicolasserrano <nicolas.serrano.barcena@gmail.com>
2019-10-15 23:05:25 +0200

    Update zoom.js

--- libraries/zoom.js
+++ libraries/zoom.js
@@ -1,6 +1,9 @@
-var elements = document.getElementsByClassName('zoom_rotate');
-for (i=0; i<elements.length;i++) {
-  var zm = new Zoom(elements[i], {
-    rotate: true
-  });
+document.addEventListener("load",addZoomRotate);
+function addZoomRotate() {
+  var elements = document.getElementsByClassName('zoom_rotate');
+  for (i=0; i<elements.length;i++) {
+    var zm = new Zoom(elements[i], {
+      rotate: true
+    });
+  }
 }
