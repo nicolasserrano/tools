498b86383b237797c22a48584d25f32bc3b01c29
nicolasserrano <nicolas.serrano.barcena@gmail.com>
2019-12-01 00:14:25 +0100

    Update readTables.js

--- libraries/readTables.js
+++ libraries/readTables.js
@@ -1,8 +1,12 @@
 imagesList=[]
 function readTables() {
-  elements = document.getElementsByClassName("images")[0].closest("table").getElementsByTagName("tr")
-  for (i=1;i<elements.length;i++){
-    console.log(i + " " + elements[i].children[0].textContent  + "-" + elements[i].children[1].textContent);
+  tables = document.getElementsByClassName("images")
+  for (it=0;it<tables.length;it++){
+    elements = tables[it].closest("table").getElementsByTagName("tr")
+    console.log("table " + it);
+    for (i=1;i<elements.length;i++){
+      console.log(i + " " + elements[i].children[0].textContent  + "-" + elements[i].children[1].textContent);
+    }
   }
 }
 readTables();
