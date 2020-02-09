520db87437b596fbfb1e765cadfc5cd78d4c10a8
nicolasserrano <nicolas.serrano.barcena@gmail.com>
2020-02-09 17:25:41 +0100

    Update setIframesForPanopto.js

--- libraries/setIframesForPanopto.js
+++ libraries/setIframesForPanopto.js
@@ -2,7 +2,7 @@ function setIframes() {
   var links = document.getElementsByTagName('a');
   var len = links.length;
   for(var i=0; i<len; i++) {
-    links[i].target = "_blank";
+    //links[i].target = "_blank";
     if (links[i].href.search("panopto.eu") >= 0 || links[i].classList.contains('iframe')) {
       ifr = document.createElement('iframe');
       ifr.src = links[i].href.replace("Viewer", "Embed") + "&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all";
